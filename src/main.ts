b120f684f3fda0f93bc472a2cfb6395752b28bf0
djfratello <kuba.szlendak@gmail.com>
2020-11-14 16:07:09 +0100

    2 files modified

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import "./polyfills";
 
+import { AngularFireModule } from "angularfire2";
+import { AngularFireAuthModule } from "angularfire2/auth";
 import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
@@ -19,16 +21,28 @@ import { MenuComponent } from "./app/component/menu/menu.component";
 // Default MatFormField appearance to 'fill' as that is the new recommended approach and the
 // `legacy` and `standard` appearances are scheduled for deprecation in version 10.
 // This makes the examples that use MatFormField render the same in StackBlitz as on the docs site.
+const firebaseConfig = {
+  apiKey: "AIzaSyCgrInZwhW0cOQkEhcU-BQ4Rq9A4fj1Brs",
+  authDomain: "sharp-sandbox-271414.firebaseapp.com",
+  databaseURL: "https://sharp-sandbox-271414.firebaseio.com",
+  projectId: "sharp-sandbox-271414",
+  storageBucket: "sharp-sandbox-271414.appspot.com",
+  messagingSenderId: "286562936358",
+  appId: "1:286562936358:web:c800dc3d6544afe3f1e8e7",
+  measurementId: "G-DYVZ5GC95C"
+};
 
 @NgModule({
   imports: [
+    AngularFireModule.initializeApp(firebaseConfig),
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
     HttpClientModule,
     DemoMaterialModule,
     MatNativeDateModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    AngularFireAuthModule
   ],
   declarations: [pwaApp, AccountComponent, MenuComponent],
   bootstrap: [pwaApp],
